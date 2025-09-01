4b12de9d9251bf99b2e950e12e669550d3143862
othman-shbeir <91564848+othman-shbeir@users.noreply.github.com>
2025-09-01 09:10:42 +0000

    Deploying to gh-pages from @ othman-shbeir/othman-shbeir.github.io@e9a22ebac2d903827513444cced0cdf54bf0d956 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "Teaching experience and materials from courses I assisted.",
           section: "Navigation",
           handler: () => {
