9e4cd020e316b7c8525e809f5c03bf5711a3a52c
othman-shbeir <91564848+othman-shbeir@users.noreply.github.com>
2025-09-13 08:52:25 +0000

    Deploying to gh-pages from @ othman-shbeir/othman-shbeir.github.io@b9d1c81586d588db8b6f33b3e38f3187d37ef43e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,9 +70,9 @@ ninja.data = [{
           description: "Hackathon project (Udacity Palestine Launchpad). Predicts student outcomes and flags at-risk students with an explainable ML pipeline and a Flask web app.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-us-bikeshare-data-analysis",
+          title: 'US Bikeshare Data Analysis',
+          description: "Built for the Udacity–Google Palestine Launchpad Data Science Nanodegree (PDSND). Interactive terminal app for exploring Chicago, NYC, and Washington bikeshare data with flexible time filters and rich stats.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
